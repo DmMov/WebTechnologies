5c642e1ce093d1b26e1c8e2c38635796aea1d2de
Dm Mov <movchanyukd@gmail.com>
2019-05-16 14:23:01 +0300

    some refactoring

--- Front/src/assets/constants/redirectCheck.js
+++ Front/src/assets/constants/redirectCheck.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Page from '../../components/_reusable/Page';
-
+import { Redirect } from 'react-router-dom';
 export const redirectCheck = (condition, { Component, pageClass, from, to }) => {
    if(condition) {
       return (
